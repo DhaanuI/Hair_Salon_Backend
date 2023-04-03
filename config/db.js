912f11de9aafe2b4bae59fbee7a5b254ecab7a77
Aman Kumar <106810850+Amanmandal-M@users.noreply.github.com>
2023-04-03 17:12:04 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true)
-
-const dbconnetion = mongoose.connect(process.env.MONGOURL)
-
-module.exports = {dbconnetion}
